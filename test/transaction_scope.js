ad3481012237b3324d5b06d4713c8ff1c0e207ac
Emmett Young <emmettyoung92@gmail.com>
2017-05-01 00:34:45 +0800

    Added transaction scope helper

---
+++ test/transaction_scope.js
@@ -0,0 +1,21 @@
+/**
+ * Created by emmet on 30/04/2017.
+ */
+const {sequelize} = require('../app/models/index');
+
+/**
+ * Opens a transaction before each tests and rollbacks it back after each tests.
+ * Saves the transaction as a global variable.
+ */
+module.exports = () =>
+{
+  // Create unmanaged transaction.
+  global.beforeEach(() => sequelize.transaction().then(transaction =>
+  {
+    // Store the transaction to be used in after each.
+    global.transaction = transaction;
+  }));
+
+  // Rolls back the transaction.
+  global.afterEach(() => global.transaction.rollback());
+};
