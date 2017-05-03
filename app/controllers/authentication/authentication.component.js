fc525ba8738b59f18ce82f9016aaaa8699d52daf
Emmett Young <emmettyoung92@gmail.com>
2017-05-03 23:33:53 +0800

    Added authentication test on controller level

--- app/controllers/authentication/authentication.component.js
+++ app/controllers/authentication/authentication.component.js
@@ -1,17 +1,19 @@
 angular.module('authentication')
   .component('logIn', {
     templateUrl: './controllers/authentication/authentication.template.html',
-    controller: ['Loader', function(Loader)
-    {
-      this.login = event =>
-      {
-        Loader.perform(event.target, () =>
-        {
+    controller: ['Loader', '$scope', function (Loader, $scope) {
+      /**
+       * Login method of the controller.
+       * @param event Contains the event object.
+       * @returns {Promise}.
+       */
+      this.login = event => {
+        return Loader.perform(event.target, () => {
           return require('../../models/domain/authentication')
             .attempt(this.username, this.password)
-            .then(() => alert("Log-in successful!"))
-            .catch(() => alert("Log-in failed!"));
-        });
+        }).then(() => this.isAuthenticated = true)
+          .catch(() => this.isAuthenticated = false)
+          .then(() => $scope.$apply());
       };
     }]
   });
---
+++ test/integration/controllers/authentication_test.js
@@ -0,0 +1,42 @@
+/**
+ * Test Helpers
+ */
+const {expect} = require('chai');
+const {ngModule, inject} = require('../../helpers/angular_test_setup');
+
+/**
+ * System Under Test
+ */
+require('../../../app/controllers/authentication/authentication.module');
+require('../../../app/controllers/authentication/authentication.component');
+
+/**
+ * Specs
+ */
+describe('Authentication Angular Controller', function(){
+
+  const USERNAME = "admin";
+  const PASSWORD = "admin";
+  let controller;
+
+  beforeEach(ngModule('authentication'));
+  beforeEach(inject($componentController => controller = $componentController('logIn')));
+
+  it("should be able to log-in successfully with correct credentials", function(done){
+    controller.username = USERNAME;
+    controller.password = PASSWORD;
+    controller.login({target: {disabled: false}}).then(() => {
+      expect(controller.isAuthenticated).to.be.true;
+      done();
+    });
+  });
+
+  it("should be able to restrict invalid user credentials", function(done){
+    controller.username = USERNAME + "wrong-username";
+    controller.password = PASSWORD + "wrong-password";
+    controller.login({target: {disabled: false}}).then(() => {
+      expect(controller.isAuthenticated).to.be.false;
+      done();
+    });
+  });
+});
