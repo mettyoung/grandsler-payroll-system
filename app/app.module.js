e3c933c8452e6fa242102cbd5c27a15f9bf4e307
Emmett Young <emmettyoung92@gmail.com>
2017-05-03 23:56:52 +0800

    Reformatted app.module

--- app/app.module.js
+++ app/app.module.js
@@ -1,20 +1,26 @@
+/**
+ * Components
+ */
 require('./controllers/authentication/authentication.module');
 require('./controllers/authentication/authentication.component');
 
+/**
+ * Router
+ */
 angular.module('grandsler-payroll-system', [
   'ngRoute',
   'authentication'
-]).config(['$locationProvider', '$routeProvider',
-  ($locationProvider, $routeProvider) => {
-    $locationProvider.hashPrefix('!');
+]).config(['$locationProvider', '$routeProvider', ($locationProvider, $routeProvider) => {
+  $locationProvider.hashPrefix('!');
+  $routeProvider
+    .when('/log-in', {
+      template: '<log-in></log-in>'
+    })
+    .otherwise('/log-in');
+}]);
 
-    $routeProvider
-      .when('/log-in', {
-        template: '<log-in></log-in>'
-      })
-      .otherwise('/log-in');
-  }
-]);
-
+/**
+ * For live-reload.
+ */
 if (process.env.NODE_ENV === 'development')
   require('electron-connect').client.create();
