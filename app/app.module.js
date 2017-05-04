f5c9a92507d78eb3f237c62ab315f1b627f654a7
Emmett Young <emmettyoung92@gmail.com>
2017-05-05 01:22:16 +0800

    Added header toolbar with three (3) navigations to change password, salary criteria and time shift registry

--- app/app.module.js
+++ app/app.module.js
@@ -5,18 +5,27 @@ require('./controllers/authentication/authentication.module');
 require('./controllers/authentication/authentication.component');
 
 /**
- * Router
+ * Various App Configuration
  */
 angular.module('grandsler-payroll-system', [
   'ngRoute',
-  'authentication'
-]).config(['$locationProvider', '$routeProvider', ($locationProvider, $routeProvider) => {
+  'authentication',
+  'ngMaterial'
+]).config(['$locationProvider', '$routeProvider', '$mdIconProvider', ($locationProvider, $routeProvider, $mdIconProvider) => {
+
+  // Just following a good convention.
   $locationProvider.hashPrefix('!');
+
+  // Main Router Logic
   $routeProvider
     .when('/log-in', {
       template: '<log-in></log-in>'
     })
     .otherwise('/log-in');
+
+  // Icon Provider
+  $mdIconProvider.iconSet('action', '../node_modules/material-design-icons/sprites/svg-sprite/svg-sprite-action.svg');
+  $mdIconProvider.iconSet('social', '../node_modules/material-design-icons/sprites/svg-sprite/svg-sprite-social.svg');
 }]);
 
 /**
