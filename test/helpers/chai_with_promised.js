e8264caec308546f7b0c137634ed82f7130d92c5
Emmett Young <emmettyoung92@gmail.com>
2017-05-05 23:37:31 +0800

    Refactored authentication test

---
+++ test/helpers/chai_with_promised.js
@@ -0,0 +1,7 @@
+/**
+ * Created by emmet on 05/05/2017.
+ */
+const chai = require('chai');
+chai.use(require('chai-as-promised'));
+chai.should();
+global.expect = chai.expect;
--- test/integration/models/domain/authentication_test.js
+++ test/integration/models/domain/authentication_test.js
@@ -1,10 +1,10 @@
 /**
  * Test Helpers
  */
-const {expect} = require('chai');
+require('../../../helpers/chai_with_promised');
+const transactionScope = require('../../../helpers/transaction_scope');
 const moment = require('moment');
 const {User, Employee, Position, TimeShift, SALARY_CRITERION, Employment} = require('../../../../app/models/persistence/index');
-const transactionScope = require('../../../helpers/transaction_scope');
 
 /**
  * System Under Test
@@ -16,114 +16,140 @@ const auth = require('../../../../app/models/domain/authentication');
  */
 describe('Authentication Module', () => {
 
-  const ID = 100;
-  const USERNAME = "dummy";
-  const PASSWORD = "dummy";
+  const ADMIN_USER = {
+    id: 1,
+    username: "admin",
+    password: "admin",
+    updated_by: 1
+  };
+
+  const DUMMY_POSITION = {
+    id: 100,
+    name: "dummy",
+    created_by: ADMIN_USER.id,
+    updated_by: ADMIN_USER.id
+  };
+
+  const DUMMY_SALARY_CRITERION = {
+    id: 100,
+    name: "Dummy Pay",
+    mark_up: "0",
+    updated_by: ADMIN_USER.id
+  };
+
+  const DUMMY_TIME_SHIFT = {
+    id: 100,
+    salary_criterion_id: 100,
+    name: "Dummy Time-shift",
+    created_by: ADMIN_USER.id,
+    updated_by: ADMIN_USER.id
+  };
+
+  const DUMMY_EMPLOYEE = {
+    id: 100,
+    position_id: 100,
+    time_shift_id: 100,
+    user_id: ADMIN_USER.id,
+    employee_type: 'Time-based',
+    last_name: "world",
+    first_name: "hello",
+    gender: 'Male',
+    created_by: ADMIN_USER.id,
+    updated_by: ADMIN_USER.id
+  };
 
   transactionScope();
-
-  beforeEach(() => {
-    return User.create({id: ID, username: USERNAME, is_enabled: 1, password: PASSWORD, updated_by: ID}, {transaction: transaction});
-  });
 
   afterEach(() => {
     auth.user = null;
   });
 
-  it("should authenticate user against the database", () =>
-  {
-    return auth.attempt(USERNAME, PASSWORD, {transaction: transaction}).then(user => {
-      expect(user).to.deep.include({
-        id: ID,
-        username: USERNAME,
-        password: PASSWORD,
-        updated_by: ID
-      });
-    });
+  it("should authenticate user against the database", () => {
+    return auth.attempt(ADMIN_USER.username, ADMIN_USER.password, {transaction: transaction})
+      .should.eventually.deep.include(ADMIN_USER);
   });
 
   it("should not authenticate the user if username and password do not match any user entries against the database", done => {
-    auth.attempt(USERNAME, "wrong-password", {transaction: transaction}).catch(() =>
-      auth.attempt("wrong-username", PASSWORD, {transaction: transaction}).catch(() =>
+    auth.attempt(ADMIN_USER.username, "wrong-password", {transaction: transaction}).catch(() =>
+      auth.attempt("wrong-username", ADMIN_USER.password, {transaction: transaction}).catch(() =>
         auth.attempt("wrong-username", "wrong-password", {transaction: transaction}).catch(() => done())
       )
     );
   });
 
-  it("should store the authenticated user in the singleton authentication instance", () =>
-  {
+  it("should store the authenticated user in the singleton authentication instance", () => {
     expect(auth.user).to.equal(null);
-    return auth.attempt(USERNAME, PASSWORD, {transaction: transaction}).then(user => {
-      expect(auth.user).to.deep.include({
-        id: ID,
-        username: USERNAME,
-        password: PASSWORD,
-        updated_by: ID
-      });
-    });
+    return auth.attempt(ADMIN_USER.username, ADMIN_USER.password, {transaction: transaction})
+      .should.eventually.deep.include(ADMIN_USER);
   });
 
   it("should not authenticate user if is_enabled is set to 0", done => {
-    User.findOne({where: {id: ID}, transaction: transaction}).then(user => {
+    User.findOne({where: {id: ADMIN_USER.id}, transaction: transaction}).then(user => {
       user.is_enabled = false;
-      user.save({transaction: transaction}).then(() =>
-      {
-        auth.attempt(USERNAME, PASSWORD, {transaction: transaction}).catch(() => {
+      user.save({transaction: transaction}).then(() => {
+        auth.attempt(ADMIN_USER.username, ADMIN_USER.password, {transaction: transaction}).catch(() => {
           done();
         });
       });
     });
   });
 
-  it("should authenticate user if employee is hired with undefined release date and its user account is still active", done => {
-    Position.create({id: ID, name: "Accountant", created_by: ID, updated_by: ID}, {transaction: transaction})
-      .then(position => SALARY_CRITERION.create({id: ID, name: "Dummy Pay", mark_up: "0", updated_by: ID}, {transaction: transaction}))
-      .then(SALARY_CRITERION => TimeShift.create({id: ID, salary_criterion_id: ID, name: "Dummy Time-shift", created_by: ID, updated_by: ID},
-        {transaction: transaction}))
-      .then(timeShift => Employee.create({id: ID, position_id: ID, time_shift_id: ID, user_id: ID, employee_type: 'Time-based',
-          last_name: "world", first_name: "hello", gender: 'Male', created_by: ID, updated_by: ID},
-        {transaction: transaction}))
-      .then(employee => Employment.create({id: ID, employee_id: ID, date_hired: new Date(), date_released: null, created_by: ID,
-        updated_by: ID}, {transaction: transaction}))
-      .then(() =>
-      {
-        auth.attempt(USERNAME, PASSWORD, {transaction: transaction}).then(() => {
-          done();
-        });
-      });
+  it("should authenticate user if employee is hired with undefined release date and its user account is still active", () => {
+    const DUMMY_EMPLOYMENT = {
+      id: 100,
+      employee_id: 100,
+      date_hired: new Date(),
+      date_released: null,
+      created_by: ADMIN_USER.id,
+      updated_by: ADMIN_USER.id
+    };
+
+    return Position.create(DUMMY_POSITION, {transaction: transaction})
+      .then(position => SALARY_CRITERION.create(DUMMY_SALARY_CRITERION, {transaction: transaction}))
+      .then(SALARY_CRITERION => TimeShift.create(DUMMY_TIME_SHIFT, {transaction: transaction}))
+      .then(timeShift => Employee.create(DUMMY_EMPLOYEE, {transaction: transaction}))
+      .then(employee => Employment.create(DUMMY_EMPLOYMENT, {transaction: transaction}))
+      .then(() => auth.attempt(ADMIN_USER.username, ADMIN_USER.password, {transaction: transaction}))
+      .should.eventually.deep.include(ADMIN_USER);
   });
 
-  it("should authenticate user if employee is hired with defined release date and its user account is still active", done => {
-    Position.create({id: ID, name: "Accountant", created_by: ID, updated_by: ID}, {transaction: transaction})
-      .then(position => SALARY_CRITERION.create({id: ID, name: "Dummy Pay", mark_up: "0", updated_by: ID}, {transaction: transaction}))
-      .then(SALARY_CRITERION => TimeShift.create({id: ID, salary_criterion_id: ID, name: "Dummy Time-shift", created_by: ID, updated_by: ID},
-        {transaction: transaction}))
-      .then(timeShift => Employee.create({id: ID, position_id: ID, time_shift_id: ID, user_id: ID, employee_type: 'Time-based',
-          last_name: "world", first_name: "hello", gender: 'Male', created_by: ID, updated_by: ID},
-        {transaction: transaction}))
-      .then(employee => Employment.create({id: ID, employee_id: ID, date_hired: new Date(), date_released: moment().add(1, 'days').toDate(), created_by: ID,
-        updated_by: ID}, {transaction: transaction}))
-      .then(() =>
-      {
-        auth.attempt(USERNAME, PASSWORD, {transaction: transaction}).then(() => {
-          done();
-        });
-      });
+  it("should authenticate user if employee is hired with defined release date and its user account is still active", () => {
+    const DUMMY_EMPLOYMENT = {
+      id: 100,
+      employee_id: 100,
+      date_hired: new Date(),
+      date_released: moment().add(1, 'days').toDate(),
+      created_by: ADMIN_USER.id,
+      updated_by: ADMIN_USER.id
+    };
+
+    return Position.create(DUMMY_POSITION, {transaction: transaction})
+      .then(position => SALARY_CRITERION.create(DUMMY_SALARY_CRITERION, {transaction: transaction}))
+      .then(SALARY_CRITERION => TimeShift.create(DUMMY_TIME_SHIFT, {transaction: transaction}))
+      .then(timeShift => Employee.create(DUMMY_EMPLOYEE, {transaction: transaction}))
+      .then(employee => Employment.create(DUMMY_EMPLOYMENT, {transaction: transaction}))
+      .then(() => auth.attempt(ADMIN_USER.username, ADMIN_USER.password, {transaction: transaction}))
+      .should.eventually.deep.include(ADMIN_USER);
   });
 
   it("should not authenticate user if employee is not hired but its user account is still active", done => {
-    Position.create({id: ID, name: "Accountant", created_by: ID, updated_by: ID}, {transaction: transaction})
-      .then(position => SALARY_CRITERION.create({id: ID, name: "Dummy Pay", mark_up: "0", updated_by: ID}, {transaction: transaction}))
-      .then(SALARY_CRITERION => TimeShift.create({id: ID, salary_criterion_id: ID, name: "Dummy Time-shift", created_by: ID, updated_by: ID},
-        {transaction: transaction}))
-      .then(timeShift => Employee.create({id: ID, position_id: ID, time_shift_id: ID, user_id: ID, employee_type: 'Time-based',
-          last_name: "world", first_name: "hello", gender: 'Male', created_by: ID, updated_by: ID},
-        {transaction: transaction}))
-      .then(employee => Employment.create({id: ID, employee_id: ID, date_hired: moment().add(1, 'days').toDate(), date_released: null, created_by: ID,
-        updated_by: ID}, {transaction: transaction}))
-      .then(() =>
-      {
-        auth.attempt(USERNAME, PASSWORD, {transaction: transaction}).catch(() => {
+    const DUMMY_EMPLOYMENT = {
+      id: 100,
+      employee_id: 100,
+      date_hired: moment().add(1, 'days').toDate(),
+      date_released: null,
+      created_by: ADMIN_USER.id,
+      updated_by: ADMIN_USER.id
+    };
+
+    Position.create(DUMMY_POSITION, {transaction: transaction})
+      .then(position => SALARY_CRITERION.create(DUMMY_SALARY_CRITERION, {transaction: transaction}))
+      .then(SALARY_CRITERION => TimeShift.create(DUMMY_TIME_SHIFT, {transaction: transaction}))
+      .then(timeShift => Employee.create(DUMMY_EMPLOYEE, {transaction: transaction}))
+      .then(employee => Employment.create(DUMMY_EMPLOYMENT, {transaction: transaction}))
+      .then(() => auth.attempt(ADMIN_USER.username, ADMIN_USER.password, {transaction: transaction}))
+      .then(() => {
+        auth.attempt(ADMIN_USER.username, ADMIN_USER.password, {transaction: transaction}).catch(() => {
           done();
         });
       });
