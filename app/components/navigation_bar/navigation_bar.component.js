1ab12aba8477a93973adf5c6c9b5c78821432096
Emmett Young <emmettyoung92@gmail.com>
2017-05-19 17:44:22 +0800

    Created a basic navigation_bar

---
+++ app/components/navigation_bar/navigation_bar.component.js
@@ -0,0 +1,7 @@
+angular.module('navigation-bar')
+  .component('navigationBar', {
+    templateUrl: './components/navigation_bar/navigation_bar.template.html',
+    controller: function()
+    {
+    }
+  });
