da816bb1504212fe932582afdaedc2b5ec477c5d
Emmett Young <emmettyoung92@gmail.com>
2017-05-19 17:23:38 +0800

    Added navigation bar

--- app/components/navigation_bar/navigation_bar.component.js
+++ app/components/navigation_bar/navigation_bar.component.js
@@ -10,11 +10,15 @@ angular.module('navigation-bar')
       };
 
       $scope.openSalaryCriteriaDialog = function() {
-
+        $mdDialog.show({
+          template: '<md-dialog aria-label="Salary criteria registry dialog." flex="40"><salary-criteria-registry></salary-criteria-registry></md-dialog>'
+        });
       };
 
       $scope.openTimeShiftRegistryDialog = function() {
-
+        $mdDialog.show({
+          template: '<md-dialog aria-label="Time-shift registry dialog." flex="80"><time-shift-registry></time-shift-registry></md-dialog>'
+        });
       };
     }
   });
--- app/components/navigation_bar/navigation_bar.module.js
+++ app/components/navigation_bar/navigation_bar.module.js
@@ -1,3 +1,8 @@
 require('../change_password/change_password.module');
 require('../change_password/change_password.component');
-angular.module('navigation-bar', ['change-password']);
+require('../salary_criteria_registry/salary_criteria_registry.module');
+require('../salary_criteria_registry/salary_criteria_registry.component');
+require('../time_shift_registry/time_shift_registry.module');
+require('../time_shift_registry/time_shift_registry.component');
+require('../time_shift_registry/time_shift_registry.config');
+angular.module('navigation-bar', ['change-password', 'salary-criteria-registry', 'time-shift-registry']);
