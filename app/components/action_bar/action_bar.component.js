d3466b6ab833806edb50a8ac8484c2467bca0afb
Emmett Young <emmettyoung92@gmail.com>
2017-05-20 13:44:56 +0800

    Added title attribute to action bar component and fixed controller

--- app/components/action_bar/action_bar.component.js
+++ app/components/action_bar/action_bar.component.js
@@ -1,24 +1,30 @@
 angular.module('action-bar')
   .component('actionBar', {
     templateUrl: './components/action_bar/action_bar.template.html',
-    controller: function($scope, $mdDialog)
+    bindings: {
+      title: '='
+    },
+    controller: ['$mdDialog', function ($mdDialog)
     {
-      $scope.openChangePasswordDialog = () => {
+      this.openChangePasswordDialog = () =>
+      {
         $mdDialog.show({
           template: '<md-dialog aria-label="Change password dialog." flex="40"><change-password></change-password></md-dialog>'
         });
       };
 
-      $scope.openSalaryCriteriaDialog = function() {
+      this.openSalaryCriteriaDialog = function ()
+      {
         $mdDialog.show({
           template: '<md-dialog aria-label="Salary criteria registry dialog." flex="40"><salary-criteria-registry></salary-criteria-registry></md-dialog>'
         });
       };
 
-      $scope.openTimeShiftRegistryDialog = function() {
+      this.openTimeShiftRegistryDialog = function ()
+      {
         $mdDialog.show({
           template: '<md-dialog aria-label="Time-shift registry dialog." flex="80"><time-shift-registry></time-shift-registry></md-dialog>'
         });
       };
-    }
+    }]
   });
