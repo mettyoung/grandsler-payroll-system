9b70fb3356ae5d882974ab68a8cbd2d271ced187
Emmett Young <emmettyoung92@gmail.com>
2017-05-24 21:02:48 +0800

    Added validation to employee form

--- app/components/employee_management/employee_management.module.js
+++ app/components/employee_management/employee_management.module.js
@@ -1,7 +1,10 @@
+require('../../directives/front_end_validations.js');
+require('../../services/notifier/notifier.module');
+require('../../services/notifier/notifier.service');
 require('../../services/crud_handler/crud_handler.module');
 require('../../services/crud_handler/crud_handler.service');
 require('../../services/model_provider/model_provider.module');
 require('../../services/model_provider/model_provider.service');
 require('../../components/position_registry/position_registry.module');
 require('../../components/position_registry/position_registry.component');
-angular.module('employee-management', ['crud-handler', 'model-provider', 'md.data.table', 'position-registry']);
+angular.module('employee-management', ['front-end-validations', 'notifier', 'crud-handler', 'model-provider', 'md.data.table', 'position-registry']);
