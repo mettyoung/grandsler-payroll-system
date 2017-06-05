05a3fbb1071f020ba39decf82b98f5675372eef0
Emmett Young <emmettyoung92@gmail.com>
2017-06-06 00:40:15 +0800

    Added config for color, operation and size registry

---
+++ app/components/size_registry/size_registry.config.js
@@ -0,0 +1,6 @@
+angular.module('size-registry')
+  .config(['$mdIconProvider', $mdIconProvider =>
+  {
+    $mdIconProvider.iconSet('action', '../node_modules/material-design-icons/sprites/svg-sprite/svg-sprite-action.svg');
+    $mdIconProvider.iconSet('content', '../node_modules/material-design-icons/sprites/svg-sprite/svg-sprite-content.svg');
+  }]);
