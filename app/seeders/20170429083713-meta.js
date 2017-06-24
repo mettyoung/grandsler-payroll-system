d83acc8c9bd119a0bec6c287a6245d5aa0f53207
Emmett Young <emmettyoung92@gmail.com>
2017-06-24 23:14:45 +0800

    [Settings] Added tolerable_threshold initial value in seeder.

--- app/seeders/20170429083713-meta.js
+++ app/seeders/20170429083713-meta.js
@@ -1,14 +1,21 @@
 'use strict';
 
 module.exports = {
-  up: function (queryInterface, Sequelize) {
-    return queryInterface.bulkInsert('meta', [{
-      "particular": "version",
-      "value": "1.0.0"
-    }]);
+  up: function (queryInterface, Sequelize)
+  {
+    return queryInterface.bulkInsert('meta', [
+      {
+        "particular": "version",
+        "value": "1.0.0"
+      },
+      {
+        "particular": "tolerable_threshold",
+        "value": "5"
+      }]);
   },
 
-  down: function (queryInterface, Sequelize) {
+  down: function (queryInterface, Sequelize)
+  {
     return queryInterface.bulkDelete('meta', {"particular": "version"});
   }
 };
